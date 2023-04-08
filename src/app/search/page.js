4db7d52bd8dd9ff5553eb2fd50332006841eabcb
NadeemChoudhary <nadeemchoudhary37@gmail.com>
2023-04-08 12:50:11 +0530

    push 1

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,6 +1,5 @@
 
 import SearchHeader from '@/Components/Search/SearchHeader'
-import Head from 'next/head'
 import React from 'react'
 
 
@@ -9,11 +8,6 @@ export default function page({ searchParams }) {
 
   return (
     <>
-
-      <Head>
-        <title>hello</title>
-        {/* <title>{Data?.queries.nextPage[0].searchTerm}- Google Search</title> */}
-      </Head>
       <SearchHeader param={searchParams} />
     </>
   )
