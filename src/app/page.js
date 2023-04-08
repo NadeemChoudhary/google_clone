9d4e5b145228d4c7f20d6cabf56fcb03b7d7ee95
NadeemChoudhary <nadeemchoudhary37@gmail.com>
2023-04-08 12:52:31 +0530

    push 2

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,7 @@ import styles from './page.module.css'
 import Header from '@/Components/Header'
 import Body from '@/Components/Body'
 
-
-export async function  getServerSideProps() {
-  const data = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAJ_TRxLePr0fo7IPd1mTWyUggQZ-AQ5qk&cx=561f6a3ff44b4403e&q=graphketing');
-  let NewData = await data.json();
-  console.log(NewData , "newDAta")
-  return {
-    props: {
-      result: NewData
-    }
-  }
-
-}
-export default function Home({result}) {
-  console.log(result , 'ooo')
+export default function Home() {
   return (
 
     <>
